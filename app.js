506f9ffefdba0bba8bbe127619ebe47541435a7c
Johanna Farace <Johanna.farace@gmail.com>
2019-07-07 10:56:33 +0200

    add value

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ var BudgetController = (function(){
 
 var UIController = (function(){
 
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+  
+  return {
+    getInput : function(){
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, //Will be either inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMString: function(){
+      return DOMStrings;
+    }
+  }
+
 
 
 })();
 
 var controller = (function(budgetCtrl, UICtrl){
 
+  var DOM = UICtrl.getDOMString();
+  
 var crtlAddItem = function(){
-  console.log('it works');
+  var input = UICtrl.getInput();
+  console.log(input);
 }
 
-document.querySelector('.add__btn').addEventListener('click', crtlAddItem);
+document.querySelector(DOM.inputBtn).addEventListener('click', crtlAddItem);
 
 document.addEventListener('keypress', function(e){
   if(e.keyCode === 13 || e.which === 13){
